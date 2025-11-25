cc302304a654181b00a665444248a23432de7c61
Sulfurbottom <397love@gmail.com>
2025-11-25 13:16:31 +0900

    feat: Initialize Shadcn UI with a Button component, Tailwind CSS utilities, and path aliases.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <div className='flex flex-col items-center justify-center h-screen w-screen'>
       <h1 className='text-3xl font-bold underline'>OverTheWall Schedule</h1>
+      <Button>Button</Button>
     </div>
   )
 }
