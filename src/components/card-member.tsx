2a9d60f833171360ea0c77240828cbb34a8c0f15
Sulfurbottom <397love@gmail.com>
2025-11-26 14:58:41 +0900

    feat: Add CardMember component for displaying member information.

--- src/components/card-member.tsx
+++ src/components/card-member.tsx
@@ -3,7 +3,7 @@ import type { Member } from "@/lib/types";
 export const CardMember = ({ member }: { member: Member }) => {
   return (
     <div
-      className={`flex flex-col gap-2 font-bold p-4 justify-center items-center rounded-lg`}
+      className={`flex flex-col gap-2 font-bold p-4 justify-center items-center rounded-lg min-w-[144px]`}
       style={{
         backgroundColor: member.main_color,
       }}
